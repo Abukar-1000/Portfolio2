e6aaf1f649a50dae0b97687e504c85669ce82718
Abukar-1000 <abukardjama1@gmail.com>
2024-01-14 09:17:46 -0500

    removed framer dependancy

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,4 +1,3 @@
-import { motion, AnimatePresence } from "framer-motion"; 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
